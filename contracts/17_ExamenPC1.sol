77e8413d10b50ad5cd8e50db910786742633a61b
Pierreluiggi Zevallos Bocanegra <pierzevallosbocanegra3@gmail.com>
2026-07-05 19:12:13 -0500

    Parte 1

---
+++ contracts/17_ExamenPC1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Universidad000240849 {
+
+    struct Estudiante {
+        uint256 id;
+        string nombre;
+        string carrera;
+    }
+
+    Estudiante[] public estudiantes;
+
+}
